4864adaefa97927f423990c3f6554f268cbf03c2
root <root@DESKTOP-QMQ51BS.localdomain>
2022-10-12 12:44:08 +0200

    first commit

---
+++ src/main/java/com/filicedaniele/projectFG/ProjectFgApplication.java
@@ -0,0 +1,13 @@
+package com.filicedaniele.projectFG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filicedaniele/projectFG/ProjectFgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filicedaniele.projectFG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
